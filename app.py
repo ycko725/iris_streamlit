25c7cd5072ec372a9fa04bf3f092ce31c39ba2c5
ycko725 <yuchanko725@gmail.com>
2023-03-31 15:57:58 +0900

    updated

---
+++ app.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import streamlit as st
+from eda_app import run_eda_app
+from ml_app import run_ml_app
+
+def main():
+    st.markdown("Hello World")
+    menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "탐색적 자료 분석":
+        run_eda_app()
+    elif choice == "머신러닝":
+        run_ml_app()
+    else:
+        st.subheader("About")
+
+if __name__ == "__main__":
+    main()
